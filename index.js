7848c146aa784022da1559b08eda5187ea754520
Thales Farias <ths.farias64@hotmail.com>
2023-08-23 23:38:38 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const reviews = require('./review-2021.json');
+const axios = require('axios');
+var fs = require('fs');
+
+const headers = {
+  'Content-type': 'application/json',
+  Authorization: "Basic NDIzMy0xYVhLNVVRdzpleXdHRERJakx2dGs3Vjc3dkdJeWlGbkJjQ0hBMWJtcm9TeEdtNWdjSjFUSw=="
+}
+
+const allFellings = [];
+async function fellings() {
+  for (let index = 18402; index < reviews.length; index++) {
+    try {
+      const element = reviews[index];
+
+      const response = await axios.post("https://api.gotit.ai/NLU/v1.5/Analyze",
+        element, { headers });
+      allFellings.push(response.data.sentiment);
+
+    } catch (error) {
+      fs.writeFileSync('./feelings-2021-8.json', JSON.stringify(allFellings))
+      console.log(`COM ERROR -- ${index}`);
+    }
+    console.log(`SEM ERROR -- ${index}`);
+    fs.writeFileSync('./feelings-2021-8.json', JSON.stringify(allFellings))
+  };
+};
+
+fellings();
